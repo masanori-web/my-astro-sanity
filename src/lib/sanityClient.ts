7f97cb337f0d4e855b1946a427a3bee221ae0e16
gyomukato <kato-masanori@rakuwa.or.jp>
2026-03-06 16:54:27 +0900

    Add main visual to homepage

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,5 +3,5 @@ export const client = createClient({
  projectId: import.meta.env.SANITY_PROJECT_ID,
  dataset: import.meta.env.SANITY_DATASET,
  apiVersion: "2026-03-04",
- useCdn: true,
+ useCdn: false,
 });
